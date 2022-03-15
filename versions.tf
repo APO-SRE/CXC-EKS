bcb4098e34f0d1a475a5650ce57979f0c872061b
Ed Barberis <ed.barberis@gmail.com>
2022-03-15 18:13:55 -0400

    Updated Terraform code and documentation with latest changes.

--- versions.tf
+++ versions.tf
@@ -1,20 +1,15 @@
 # Terraform ----------------------------------------------------------------------------------------
 terraform {
-  required_version = ">= 1.1.6"
+  required_version = ">= 1.1.7"
 
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = ">= 4.3"
+      version = ">= 4.5"
     }
 
     kubernetes = {
       version = "~> 2.8"
-    }
-
-    local = {
-      source = "hashicorp/local"
-      version = ">= 2.1"
     }
 
     null = {
@@ -25,11 +20,6 @@ terraform {
     random = {
       source = "hashicorp/random"
       version = ">= 3.1"
-    }
-
-    template = {
-      source = "hashicorp/template"
-      version = ">= 2.2"
     }
   }
 }
